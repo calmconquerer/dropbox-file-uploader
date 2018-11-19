984a5ca0e7d20465d1bb21bd73bd43e63cd99ac0
calmconquerer <wasiullah.khan21@gmail.com>
2018-11-19 18:27:47 +0500

    Added some documentation and the fluentd logger

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import dropbox
 import config
 import os
+from fluent import sender
+from fluent import event
 
+# Token taken from the configuration file
+dbx = dropbox.Dropbox(config.exports['token'])
 
-token = config.exports['token']
-dbx = dropbox.Dropbox(token)
-
-
-sourceFolder = config.exports['sourceFolder']
-targetFolder = config.exports['targetFolder']
-
+# Setup fluentd for logging
+sender.setup('fluentd.test', host='localhost', port=24224)
 
 chunk_size = config.exports['chunkSize'] * 1024 * 1024
 max_chunk_size = 2 * 1024 * 1024
 
-
-for dirpath, dirnames, files in os.walk(sourceFolder):
+'''
+    - Basically it traverses over the source folder and then takes each file in the source folder and uploads it to 
+      dropbox directly if it's less than the max chunk size (max chunk size is 150 in the dropbox api but for my 
+      convenience I made it 2(poor internet connection)).
+    
+    - If the file is larger than the max chunk size, it creates an upload session for the file to get uploaded in small 
+      chunks.
+    
+    - Exception handling in this is somewhat poor but I'll definitely improve it.
+    
+    - Added the fluentd logger in the Exception handling block to log the errors since the app will run in the 
+      background
+      
+'''
+for dirpath, dirnames, files in os.walk(config.exports['sourceFolder']):
     for file in files:
         try:
             file_path = os.path.join(dirpath, file)
-            destination_path = os.path.join(targetFolder, file)
-            print('Uploading %s to %s' % (file_path, destination_path))
+            destination_path = os.path.join(config.exports['targetFolder'], file)
+            print('Uploading {} to {}'.format(file_path, destination_path))
             with open(file_path, 'rb') as f:
                 # if file == '.DS_Store':
                 #     pass
@@ -45,6 +57,11 @@ for dirpath, dirnames, files in os.walk(sourceFolder):
             os.unlink(file_path)
             f.close()
         except Exception as err:
-                print("Failed to upload %s\n%s" % (file, err))
+            print("Failed to upload {}\n{}".format(file, err))
+            event.Event('follow', {
+                'Failed to upload ': file,
+                'Error': err
+            })
+
 print("Finished upload.")
 
