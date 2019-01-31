ee8b1d55497ce64405b9ba8f869eddf9ba78198b
calmconquerer <wasiullah.khan21@gmail.com>
2019-01-31 12:31:07 +0500

    Added the requirements file for the app

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import config
 import os
 from fluent import sender
 from fluent import event
+from tqdm import tqdm
 
 # Token taken from the configuration file
 dbx = dropbox.Dropbox(config.exports['token'])
@@ -15,7 +16,7 @@ max_chunk_size = 2 * 1024 * 1024
 
 '''
     - Basically it traverses over the source folder and then takes each file in the source folder and uploads it to 
-      dropbox directly if it's less than the max chunk size (max chunk size is 150 in the dropbox api but for my 
+      DropBox directly if it's less than the max chunk size (max chunk size is 150 in the dropbox api but for my 
       convenience I made it 2(poor internet connection)).
     
     - If the file is larger than the max chunk size, it creates an upload session for the file to get uploaded in small 
@@ -27,18 +28,17 @@ max_chunk_size = 2 * 1024 * 1024
       background
       
 '''
+
 for dirpath, dirnames, files in os.walk(config.exports['sourceFolder']):
-    for file in files:
+    for file in tqdm(files):
         try:
             file_path = os.path.join(dirpath, file)
             destination_path = os.path.join(config.exports['targetFolder'], file)
             print('Uploading {} to {}'.format(file_path, destination_path))
             with open(file_path, 'rb') as f:
-                # if file == '.DS_Store':
-                #     pass
                 fileSize = os.path.getsize(file_path)
                 if fileSize <= max_chunk_size:
-                    if file == '.DS_Store':
+                    if file == '.DS_Store':  # A mac user will understand
                         pass
                     else:
                         dbx.files_upload(f.read(), destination_path)
@@ -64,4 +64,3 @@ for dirpath, dirnames, files in os.walk(config.exports['sourceFolder']):
             })
 
 print("Finished upload.")
-
